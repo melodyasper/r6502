21624d400aacb72680b0d894f7f91cd8a1ff9ca2
local <local@local>
2024-02-03 18:18:36 -0500

    State rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{error, fmt::Error};
+
 #[derive(Debug)]
 enum Instruction {
     GroupOne(GroupOneInstruction),
@@ -18,14 +20,17 @@ enum GroupOneMode {
 }
 #[derive(Debug)]
 enum GroupOneInstruction {
-    ORA(GroupOneMode),	
-    AND(GroupOneMode),	
-    EOR(GroupOneMode),	
-    ADC(GroupOneMode),	
-    STA(GroupOneMode),	
-    LDA(GroupOneMode),	
-    CMP(GroupOneMode),	
-    SBC(GroupOneMode),	
+    ORA(GroupOneMode),
+    AND(GroupOneMode),
+    EOR(GroupOneMode),
+    ADC(GroupOneMode),
+    STA(GroupOneMode),
+    LDA(GroupOneMode),
+    CMP(GroupOneMode),
+    SBC(GroupOneMode),
+}
+trait Executable {
+    fn execute();
 }
 
 impl GroupOneInstruction {
@@ -41,52 +46,49 @@ impl GroupOneInstruction {
             GroupOneInstruction::SBC(_) => 1,
         }
     }
-
 }
 
 #[derive(Debug)]
 enum GroupTwoMode {
-    Immediate,         // 0b000; #immediate
-    DirectZeroPage,    // 0b001; zero page
-    Accumulator,       // 0b010; accumulator
-    DirectAbsolute,    // 0b011; absolute
-    DirectZeroPageX,   // 0b101; zero page,X
-    DirectAbsoluteX,   // 0b111; absolute,X
+    Immediate,       // 0b000; #immediate
+    DirectZeroPage,  // 0b001; zero page
+    Accumulator,     // 0b010; accumulator
+    DirectAbsolute,  // 0b011; absolute
+    DirectZeroPageX, // 0b101; zero page,X
+    DirectAbsoluteX, // 0b111; absolute,X
 }
 
 #[derive(Debug)]
 enum GroupTwoInstruction {
-    ASL(GroupTwoMode),	
-    ROL(GroupTwoMode),	
-    LSR(GroupTwoMode),	
-    ROR(GroupTwoMode),	
-    STX(GroupTwoMode),	
-    LDX(GroupTwoMode),	
-    DEC(GroupTwoMode),	
-    INC(GroupTwoMode),	
+    ASL(GroupTwoMode),
+    ROL(GroupTwoMode),
+    LSR(GroupTwoMode),
+    ROR(GroupTwoMode),
+    STX(GroupTwoMode),
+    LDX(GroupTwoMode),
+    DEC(GroupTwoMode),
+    INC(GroupTwoMode),
 }
 
 #[derive(Debug)]
 enum GroupThreeMode {
-    Immediate,         // 0b000; #immediate
-    DirectZeroPage,    // 0b001; zero page
-    DirectAbsolute,    // 0b011; absolute
-    DirectZeroPageX,   // 0b101; zero page,X
-    DirectAbsoluteX,   // 0b111; absolute,X
+    Immediate,       // 0b000; #immediate
+    DirectZeroPage,  // 0b001; zero page
+    DirectAbsolute,  // 0b011; absolute
+    DirectZeroPageX, // 0b101; zero page,X
+    DirectAbsoluteX, // 0b111; absolute,X
 }
-
 
 #[derive(Debug)]
 enum GroupThreeInstruction {
-    BIT(GroupThreeMode), // 001
-    JMP(GroupThreeMode), // 010
+    BIT(GroupThreeMode),         // 001
+    JMP(GroupThreeMode),         // 010
     JMPAbsolute(GroupThreeMode), // 011
-    STY(GroupThreeMode), // 100
-    LDY(GroupThreeMode), // 101
-    CPY(GroupThreeMode), // 110
-    CPX(GroupThreeMode), // 111
+    STY(GroupThreeMode),         // 100
+    LDY(GroupThreeMode),         // 101
+    CPY(GroupThreeMode),         // 110
+    CPX(GroupThreeMode),         // 111
 }
-
 
 impl TryFrom<u8> for GroupOneInstruction {
     type Error = ();
@@ -104,8 +106,7 @@ impl TryFrom<u8> for GroupOneInstruction {
             0b101 => GroupOneMode::DirectZeroPageX,
             0b110 => GroupOneMode::DirectAbsoluteY,
             0b111 => GroupOneMode::DirectAbsoluteX,
-            _ => return Err(())
-
+            _ => return Err(()),
         };
 
         match instruction_bits {
@@ -136,7 +137,7 @@ impl TryFrom<u8> for GroupTwoInstruction {
             0b011 => GroupTwoMode::DirectAbsolute,
             0b101 => GroupTwoMode::DirectZeroPageX,
             0b111 => GroupTwoMode::DirectAbsoluteX,
-            _ => return Err(())
+            _ => return Err(()),
         };
 
         match instruction_bits {
@@ -166,7 +167,7 @@ impl TryFrom<u8> for GroupThreeInstruction {
             0b011 => GroupThreeMode::DirectAbsolute,
             0b101 => GroupThreeMode::DirectZeroPageX,
             0b111 => GroupThreeMode::DirectAbsoluteX,
-            _ => return Err(())
+            _ => return Err(()),
         };
 
         match instruction_bits {
@@ -182,7 +183,33 @@ impl TryFrom<u8> for GroupThreeInstruction {
     }
 }
 
+struct State {
+    running: bool,
+    program_counter: usize,
+    memory: Vec<u8>,
+}
+impl State {
+    fn get_next_instruction(&mut self) -> Option<Instruction> {
+        let program_counter = self.program_counter;
+        self.program_counter += 1;
+        match self.memory.get(program_counter) {
+            Some(value) => match Instruction::try_from(*value) {
+                Ok(next_instruction) => Some(next_instruction),
+                Err(_) => None
+            },
+            None => {
+                self.running = false;
+                None
+            }
+        }
+    }
+}
 
+impl Instruction {
+    fn execute<'a>(&self, state: State) -> State {
+        return state;
+    }
+}
 
 impl TryFrom<u8> for Instruction {
     type Error = ();
@@ -192,17 +219,16 @@ impl TryFrom<u8> for Instruction {
         match group_bits {
             0b01 => Ok(Instruction::GroupOne(GroupOneInstruction::try_from(value)?)),
             0b10 => Ok(Instruction::GroupTwo(GroupTwoInstruction::try_from(value)?)),
-            0b00 => Ok(Instruction::GroupThree(GroupThreeInstruction::try_from(value)?)),
+            0b00 => Ok(Instruction::GroupThree(GroupThreeInstruction::try_from(
+                value,
+            )?)),
             _ => Err(()),
         }
     }
 }
 
-
-
-
 // 0xA5 = 10100101 ( 165 )
-// aaabbbcc. The aaa and cc bits determine the opcode, and the bbb bits determine the addressing mode. 
+// aaabbbcc. The aaa and cc bits determine the opcode, and the bbb bits determine the addressing mode.
 // bbb = 001
 // cc = 01
 // aaa = 101
@@ -217,9 +243,6 @@ impl TryFrom<u8> for Instruction {
 // 110	absolute,Y
 // 111	absolute,X
 
-
-
-
 fn main() {
     // Instructions from https://codeburst.io/an-introduction-to-6502-assembly-and-low-level-programming-7c11fa6b9cb9
     // LDA   $60
@@ -227,19 +250,22 @@ fn main() {
     // STA   $62
 
     // https://llx.com/Neil/a2/opcodes.html
-    let instructions: [u8; 6] = [0xA5, 0x60, 0x65, 0x61, 0x85, 0x62];
     let mut program_counter = 0;
-    while program_counter < instructions.len() {
-        let program_counter_value = instructions.get(program_counter).unwrap();
-        // Extract bit values early
-        match Instruction::try_from(*program_counter_value) {
-            Ok(x) => println!("Found instruction {:?}", x),
-            Err(_) => println!("Could not parse instruction")
+    let mut state = State {
+        running: true,
+        program_counter: 0,
+        memory: vec![0xA5, 0x60, 0x65, 0x61, 0x85, 0x62],
+    };
+    while state.running {
+        match state.get_next_instruction() {
+            Some(instruction) => {
+                println!("Found instruction {:?}", instruction);
+                state = instruction.execute(state);
+            }
+            None => {
+                println!("Program finished");
+            }
         }
-
-        
-        println!("{:?}", program_counter_value);
-        program_counter += 1;
     }
 
     println!("Hello, world!");
